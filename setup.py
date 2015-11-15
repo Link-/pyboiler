149be917e1f0ce5b395e1c13b01bb0a04de113ba
Omar Abou Mrad <omar.aboumrad@gmail.com>
2015-11-15 11:49:45 +0200

    minimized installer

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.2.0'
+VERSION = '0.2.0'
+DESCRIPTION = """\
+Creates a configurable python project template in a given directory."""
 
 setup(name='pyboiler',
-      version=version,
-      description="Creates a configurable python project template in a given directory.",
-      long_description="""\
-Creates a configurable python project template in a given directory.""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=DESCRIPTION,
+      classifiers=[],
       keywords='boilerplate python script configurable',
       author='Link-',
       author_email='bd@bassemdy.com',
-      scripts=['bin/pyboiler.py', 'bin/jparser.py', 'bin/jparser.pyc'],
       url='https://pypi.python.org/pypi/pyboiler',
       license='LICENSE',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=['pyboiler'],
       include_package_data=True,
       zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+
+      entry_points={
+          'console_scripts': [
+              'pyboiler = pyboiler.pyboiler:main',
+          ]
+      }
+)
