f5e85bb758e23670a63928561010b4f80d62c1ae
Omar Abou Mrad <omar.aboumrad@gmail.com>
2015-11-15 12:27:55 +0200

    code cleanups
    dropped jparser
    reduced code noise
    dropped tab/space mixing

--- pyboiler/pyboiler.py
+++ pyboiler/pyboiler.py
@@ -1,162 +1,103 @@
 #!/usr/bin/python
-# 
-#  ~~~~~~~~~~
-# | PyBoiler |
-#  ~~~~~~~~~~
-# 
-# @version: alpha-0.2.0 | python 2.7.x
-# -
-# @desc: PyBoiler is a simple python script
-# that creates the basic folder/file structure
-# for simple python projects.
-# -
-# 
-# LICENSE
-# -------
-# 
-# DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                    Version 2, December 2004
-#
-# Copyright (C) 2014 Bassem Dghaidi <bd@bassemdy.com>
-#
-# Everyone is permitted to copy and distribute verbatim or modified
-# copies of this license document, and changing it is allowed as long
-# as the name is changed.
-#
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-#
-#  0. You just DO WHAT THE FUCK YOU WANT TO.
 
-import os, sys
-from os.path import join, exists
-from distutils.util import strtobool
 import argparse
-from jparser import parse_json_structure
+from distutils.util import strtobool
+import json
+import os
 
-# Modify the folder structure as you like
-DEFAULT_FOLDER_STRUCTURE = [{'folder': 'source'},
-						# {'file': 'source/file1.txt'},
-						{'folder': 'sublime'},
-						# {'file': 'sublime/file2.txt'},
-						{'folder': 'docs'},
-						# {'file': 'docs/file3.txt'},
-						# {'file': 'docs/file4.txt'},
-						{'file': 'README.md'}]
 
-''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	--- touch(str)
-	Create an empty file in a given directory
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-'''
+DEFAULT_FOLDER_STRUCTURE = [
+	{'folder': 'source'},
+	{'folder': 'sublime'},
+	{'folder': 'docs'},
+	{'file': 'README.md'}
+]
+
+
 def touch(path):
+	"""Create an empty file in a given directory"""
 	open(path, 'a').close()
 
 
-''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	--- build_folder_structure(str, boolean)
-	Builds the folder structure in
-	a given directory
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-'''
 def build_folder_structure(project_path, file_loc, override):
-	# Test if the file location param
-	# has been passed and is not empty
-	# Strings return false if empty
+	"""Builds the folder structure in a given directory"""
+
 	if file_loc:
-		folder_structure = parse_json_structure(file_loc)
+		folder_structure = json.load(file_loc)
 	else:
 		folder_structure = DEFAULT_FOLDER_STRUCTURE
 
-	# If user chose to override the
-	# folder structure for a pre-existing
-	# folder, notify the user either way
 	if override:
-		sys.stdout.write('[NOTICE]: Overriding content of the directory: %s \n' % project_path)
-		sys.stdout.write('Creating the folder structure now... \n\n')
+		print('> Overriding content of the directory: %s' % project_path)
+		print('> Creating the folder structure now... \n')
 	else:
-		sys.stdout.write('Creating the folder the directory: %s \n\n' % project_path)
+		print('> Creating the folder the directory: %s \n' % project_path)
 
-	# Create the source dir
 	for lst in folder_structure:
 		for t, n in lst.iteritems():
 			try:
-				# Define full path
-				joint_path = join(project_path, n)
+				joint_path = os.path.join(project_path, n)
 
 				if t == 'folder':
-					# Make Directory
-					if not exists(joint_path):
+					if not os.path.exists(joint_path):
 						os.makedirs(joint_path)
 				elif t == 'file':
-					# Create file
 					touch(joint_path)
 
-				sys.stdout.write('Created: %s \n' % joint_path)
+				print('Created: %s' % joint_path)
 
 			except OSError:
-				sys.stdout.write('[ERROR]: fatal error - exiting...\n');
+				print('> Fatal error - exiting...')
 
 
-''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	--- user_yes_no_query(str)
-	Prompts the user for a choice
-	return Boolean
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-'''
 def user_yes_no_query(question):
-	sys.stdout.write('%s [y/n]:\n' % question)
+	"""Prompts the user for a choice"""
 
-	# Keep waiting for the proper answer
+	print('%s [y/n]:' % question)
+
 	while True:
 		try:
 			return strtobool(raw_input().lower())
-
 		except ValueError:
-			sys.stdout.write('Please respond with \'y\' or \'n\'.\n')
+			print("Please respond with 'y' or 'n'.")
 
 
-''' ~~~~~~~~~~
-	--- main()
-	~~~~~~~~~~
-'''
 def main():
-	# First we parse the CLI arguments passed to the script
 	args_parser = argparse.ArgumentParser()
 
-	args_parser.add_argument('-o', '--project-directory',
-						dest = 'project_path',
-						help = 'Project\'s absolute path where the structure will be created', 
-						type=str, required=True)
+	args_parser.add_argument(
+		'-o', '--project-directory',
+		dest='project_path',
+		help='Project\'s absolute path where the structure will be created',
+		type=str,
+		required=True)
 
-	args_parser.add_argument('-i', '--folder-structure',
-	                    dest = 'folder_structure',
-	                    help = 'JSON file containing the template folder/file structure to be created',
-	                    type=str, required=False)
+	args_parser.add_argument(
+		'-i', '--folder-structure',
+		dest='folder_structure',
+		help='file containing the template folder/file structure to be created',
+		type=str,
+		required=False)
 
 	received_args = args_parser.parse_args()
 
-	# We verify the received argument's validity
-	# first we check if the folder exists and 
-	# prompt for override choice
 	if os.path.exists(received_args.project_path):
-		# Warn that the folder exists and ask if override
-		override_bool = user_yes_no_query('[WARNING]: Folder already exists, do you wish to override it?')
+		override_bool = user_yes_no_query(
+			'[WARNING]: Folder already exists, do you wish to override it?')
 
-		if override_bool == False:
-			# Exit if no override is wanted
-			sys.exit('[WARNING]: Cleaning up and exiting...')
+		if not override_bool:
+			exit('[WARNING]: Cleaning up and exiting...')
 		else:
-			# Continue the code execution
-			build_folder_structure(received_args.project_path, received_args.folder_structure, True)
+			build_folder_structure(
+				received_args.project_path,
+				received_args.folder_structure,
+				True)
 	else:
-		# Continue as normal
-		build_folder_structure(received_args.project_path, received_args.folder_structure, False)
+		build_folder_structure(
+			received_args.project_path,
+			received_args.folder_structure,
+			False)
 
 
-''' ~~~~~~~~~~
-	--- Main
-	~~~~~~~~~~
-'''
 if __name__ == "__main__":
 	main()
