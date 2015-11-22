731ed7b1a42ab57044f7510a4b864b079118e38d
BD <bd@bassemdy.com>
2015-11-23 00:58:21 +0300

    .. fixed the setuptools configuration information

--- setup.py
+++ setup.py
@@ -9,7 +9,12 @@ setup(name='pyboiler',
       version=VERSION,
       description=DESCRIPTION,
       long_description=DESCRIPTION,
-      classifiers=[],
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Build Tools'
+      ],
       keywords='boilerplate python script configurable',
       author='Link-',
       author_email='bsm.dgdy@gmail.com',
@@ -18,7 +23,6 @@ setup(name='pyboiler',
       packages=['pyboiler'],
       include_package_data=True,
       zip_safe=True,
-
       entry_points={
           'console_scripts': [
               'pyboiler = pyboiler.pyboiler:main',
